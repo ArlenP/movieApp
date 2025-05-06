11bfb32dc7190a042bf9f4b7417fa6de89c0fad9
ArlenP <arlen.pena@gmail.com>
2025-05-06 12:00:14 -0600

    Inicialización del proyecto con estructura base VIPER y red

---
+++ MovieApp/Modules/TestView/TestView.swift
@@ -0,0 +1,18 @@
+//
+//  TestView.swift
+//  MovieApp
+//
+//  Created by Arlen Peña on 06/05/25.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestView()
+}
